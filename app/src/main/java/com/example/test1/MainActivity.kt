866083a10f5614bab115d0684a0004d80b980ffc
AarDG10 <aarol.dsouza@gmail.com>
2023-02-28 18:41:47 +0530

    Music Start (28-02-2023)

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.test1
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 
@@ -18,24 +18,24 @@ class MainActivity : AppCompatActivity() {
         val 5 - Chat
         */
         val card1 = findViewById<CardView>(R.id.card1)
-        card1.setOnClickListener{
-            startActivity(Intent(this,Profilepage::class.java))
+        card1.setOnClickListener {
+            startActivity(Intent(this, Profilepage::class.java))
         }
         val card2 = findViewById<CardView>(R.id.card2)
-        card2.setOnClickListener{
-            startActivity(Intent(this,Music::class.java))
+        card2.setOnClickListener {
+            startActivity(Intent(this, Music::class.java))
         }
         val card3 = findViewById<CardView>(R.id.card3)
-        card3.setOnClickListener{
-            startActivity(Intent(this,Sports::class.java))
+        card3.setOnClickListener {
+            startActivity(Intent(this, Sports::class.java))
         }
         val card4 = findViewById<CardView>(R.id.card4)
-        card4.setOnClickListener{
-            startActivity(Intent(this,Mast::class.java))
+        card4.setOnClickListener {
+            startActivity(Intent(this, Mast::class.java))
         }
         val card5 = findViewById<CardView>(R.id.card5)
-        card5.setOnClickListener{
-            startActivity(Intent(this,chat::class.java))
+        card5.setOnClickListener {
+            startActivity(Intent(this, chat::class.java))
         }
     }
 }
