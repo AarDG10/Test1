f9ee0f6d5ad6b6b4ef0e94d036a5cb00076145a5
AarDG10 <aarol.dsouza@gmail.com>
2023-04-09 11:19:07 +0530

    Music Start (28-02-2023)

--- app/src/main/java/com/example/test1/Music.kt
+++ app/src/main/java/com/example/test1/Music.kt
@@ -1,63 +1,68 @@
 package com.example.test1
+import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_music.*
+import kotlinx.android.synthetic.main.music1.*
 import java.util.*
 
 class Music : AppCompatActivity() {
-    lateinit var runnable: Runnable
-    private var handler = android.os.Handler()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music)
-        val mediaPlayer = MediaPlayer.create(this,R.raw.musicp1)
-        seekbar.progress = 0
-        seekbar.max = mediaPlayer.duration
-        //Creation of play button
-        play.setOnClickListener{
-            if(!mediaPlayer.isPlaying)
-            {
-                mediaPlayer.start()
-                //changing logo
-                play.setImageResource(R.drawable.ic_baseline_pause_24)
-            }
-            else
-            {
-                mediaPlayer.pause()
-                //changing logo
-                play.setImageResource(R.drawable.ic_baseline_play_arrow_24)
-            }
+        val buttonClick1 = findViewById<Button>(R.id.button1)
+        buttonClick1.setOnClickListener {
+            val intent = Intent(this, Music01::class.java)
+            startActivity(intent)
         }
-        //Now we will add seekbar event
-        //when we change our seekbar progress the song will change its current pos..
-        seekbar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(p0: SeekBar?, pos: Int, changed: Boolean){
-                //Now when  we change the position of seekbar the music will go to that position
-                if(changed){
-                    mediaPlayer.seekTo(pos)
-                }
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-
-            }
-        })
-        runnable = Runnable {
-            seekbar.progress = mediaPlayer.currentPosition
-            handler.postDelayed(runnable,1000)
+        val buttonClick2 = findViewById<Button>(R.id.button2)
+        buttonClick2.setOnClickListener{
+            val intent = Intent(this, Music02::class.java)
+            startActivity(intent)
         }
-        handler.postDelayed(runnable,1000)
-
-        mediaPlayer.setOnCompletionListener {
-            play.setImageResource(R.drawable.ic_baseline_play_arrow_24)
-            seekbar.progress = 0
+        val buttonClick3 = findViewById<Button>(R.id.button3)
+        buttonClick3.setOnClickListener {
+            val intent = Intent(this, Music03::class.java)
+            startActivity(intent)
         }
-
+        val buttonClick4 = findViewById<Button>(R.id.button4)
+        buttonClick4.setOnClickListener {
+            val intent = Intent(this, Music04::class.java)
+            startActivity(intent)
+        }
+        val buttonClick5 = findViewById<Button>(R.id.button5)
+        buttonClick5.setOnClickListener {
+            val intent = Intent(this, Music05::class.java)
+            startActivity(intent)
+        }
+        val buttonClick6 = findViewById<Button>(R.id.button6)
+        buttonClick6.setOnClickListener {
+            val intent = Intent(this, Music06::class.java)
+            startActivity(intent)
+        }
+        val buttonClick7 = findViewById<Button>(R.id.button7)
+        buttonClick7.setOnClickListener {
+            val intent = Intent(this, Music07::class.java)
+            startActivity(intent)
+        }
+        val buttonClick8 = findViewById<Button>(R.id.button8)
+        buttonClick8.setOnClickListener {
+            val intent = Intent(this, Music08::class.java)
+            startActivity(intent)
+        }
+        val buttonClick9 = findViewById<Button>(R.id.button9)
+        buttonClick9.setOnClickListener {
+            val intent = Intent(this, Music09::class.java)
+            startActivity(intent)
+        }
+        val buttonClick10 = findViewById<Button>(R.id.button10)
+        buttonClick10.setOnClickListener {
+            val intent = Intent(this, Music10::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/test1/Music06.kt
@@ -0,0 +1,64 @@
+package com.example.test1
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_music.*
+import kotlinx.android.synthetic.main.music1.*
+import java.util.*
+
+class Music06 : AppCompatActivity() {
+    lateinit var runnable: Runnable
+    private var handler = android.os.Handler()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.music6)
+        val mediaPlayer = MediaPlayer.create(this,R.raw.honeyjam)
+        seekbar.progress = 0
+        seekbar.max = mediaPlayer.duration
+        //Creation of play button
+        play.setOnClickListener{
+            if(!mediaPlayer.isPlaying)
+            {
+                mediaPlayer.start()
+                //changing logo
+                play.setImageResource(R.drawable.ic_baseline_pause_24)
+            }
+            else
+            {
+                mediaPlayer.pause()
+                //changing logo
+                play.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+            }
+        }
+        //Now we will add seekbar event
+        //when we change our seekbar progress the song will change its current pos..
+        seekbar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, pos: Int, changed: Boolean){
+                //Now when  we change the position of seekbar the music will go to that position
+                if(changed){
+                    mediaPlayer.seekTo(pos)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+        runnable = Runnable {
+            seekbar.progress = mediaPlayer.currentPosition
+            handler.postDelayed(runnable,1000)
+        }
+        handler.postDelayed(runnable,1000)
+
+        mediaPlayer.setOnCompletionListener {
+            play.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+            seekbar.progress = 0
+        }
+
+    }
+}
